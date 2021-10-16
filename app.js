c8800af0cb65ad679e3b4df3da98a5a290c2451e
Deniz YILDIRIM <deniz@Deniz-MacBook-Pro.local>
2021-10-16 22:23:32 +0300

    Anasayfa kartları arka planı değişiyor.

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+
+var $ = jQuery = require('jquery')(window);
+
 
 
 app.use(express.urlencoded({extended:true}));
@@ -10,7 +18,6 @@ app.use(express.static(__dirname));
 app.get("/", function (req, res) {
   res.sendFile(__dirname+"/index.html");
 })
-
 
 
 
---
+++ script.js
@@ -0,0 +1,115 @@
+
+
+// HAFTANIN KİTAPLARI
+
+$(".kart-1").hover(function(){
+  $(".kart-govde-1").css("background-color", "white");
+  $(".kart-govde-1 > .list-group > .item").addClass("siyah");
+  $(".kart-govde-1 > .list-group > .item").removeClass("beyaz");
+  }, function(){
+  $(".kart-govde-1").css("background-color", "");
+  $(".kart-govde-1 > .item").removeClass("siyah");
+  $(".kart-govde-1 > .list-group > .item").addClass("beyaz");
+
+});
+
+$(".kart-2").hover(function(){
+  $(".kart-govde-2").css("background-color", "white");
+  $(".kart-govde-2 > .list-group > .item").addClass("siyah");
+  $(".kart-govde-2 > .list-group > .item").removeClass("beyaz");
+  }, function(){
+  $(".kart-govde-2").css("background-color", "");
+  $(".kart-govde-2 > .item").removeClass("siyah");
+  $(".kart-govde-2 > .list-group > .item").addClass("beyaz");
+
+});
+
+$(".kart-3").hover(function(){
+  $(".kart-govde-3").css("background-color", "white");
+  $(".kart-govde-3 > .list-group > .item").addClass("siyah");
+  $(".kart-govde-3 > .list-group > .item").removeClass("beyaz");
+  }, function(){
+  $(".kart-govde-3").css("background-color", "");
+  $(".kart-govde-3 > .item").removeClass("siyah");
+  $(".kart-govde-3 > .list-group > .item").addClass("beyaz");
+
+});
+
+$(".kart-4").hover(function(){
+  $(".kart-govde-4").css("background-color", "white");
+  $(".kart-govde-4 > .list-group > .item").addClass("siyah");
+  $(".kart-govde-4 > .list-group > .item").removeClass("beyaz");
+  }, function(){
+  $(".kart-govde-4").css("background-color", "");
+  $(".kart-govde-4 > .item").removeClass("siyah");
+  $(".kart-govde-4 > .list-group > .item").addClass("beyaz");
+
+});
+
+$(".kart-5").hover(function(){
+  $(".kart-govde-5").css("background-color", "white");
+  $(".kart-govde-5 > .list-group > .item").addClass("siyah");
+  $(".kart-govde-5 > .list-group > .item").removeClass("beyaz");
+  }, function(){
+  $(".kart-govde-5").css("background-color", "");
+  $(".kart-govde-5 > .item").removeClass("siyah");
+  $(".kart-govde-5 > .list-group > .item").addClass("beyaz");
+
+});
+
+$(".kart-6").hover(function(){
+  $(".kart-govde-6").css("background-color", "white");
+  $(".kart-govde-6 > .list-group > .item").addClass("siyah");
+  $(".kart-govde-6 > .list-group > .item").removeClass("beyaz");
+  }, function(){
+  $(".kart-govde-6").css("background-color", "");
+  $(".kart-govde-6 > .item").removeClass("siyah");
+  $(".kart-govde-6 > .list-group > .item").addClass("beyaz");
+
+});
+
+$(".kart-7").hover(function(){
+  $(".kart-govde-7").css("background-color", "white");
+  $(".kart-govde-7 > .list-group > .item").addClass("siyah");
+  $(".kart-govde-7 > .list-group > .item").removeClass("beyaz");
+  }, function(){
+  $(".kart-govde-7").css("background-color", "");
+  $(".kart-govde-7 > .item").removeClass("siyah");
+  $(".kart-govde-7 > .list-group > .item").addClass("beyaz");
+
+});
+
+$(".kart-8").hover(function(){
+  $(".kart-govde-8").css("background-color", "white");
+  $(".kart-govde-8 > .list-group > .item").addClass("siyah");
+  $(".kart-govde-8 > .list-group > .item").removeClass("beyaz");
+  }, function(){
+  $(".kart-govde-8").css("background-color", "");
+  $(".kart-govde-8 > .item").removeClass("siyah");
+  $(".kart-govde-8 > .list-group > .item").addClass("beyaz");
+
+});
+
+$(".kart-9").hover(function(){
+  $(".kart-govde-9").css("background-color", "white");
+  $(".kart-govde-9 > .list-group > .item").addClass("siyah");
+  $(".kart-govde-9 > .list-group > .item").removeClass("beyaz");
+  }, function(){
+  $(".kart-govde-9").css("background-color", "");
+  $(".kart-govde-9 > .item").removeClass("siyah");
+  $(".kart-govde-9 > .list-group > .item").addClass("beyaz");
+
+});
+
+// HAFTANIN VİDEOSU
+
+$(".kart-10").hover(function(){
+  $(".kart-govde-10").css("background-color", "white");
+  $(".kart-10 > .kart-govde-10 > .item").addClass("siyah");
+  $(".kart-10 > .kart-govde-10 > .item").removeClass("beyaz");
+  }, function(){
+  $(".kart-govde-10").css("background-color", "");
+  $(".kart-10 > kart-gövde-10 > .item").removeClass("siyah");
+  $(".kart-10 > .kart-govde-10 > .item").addClass("beyaz");
+
+});
